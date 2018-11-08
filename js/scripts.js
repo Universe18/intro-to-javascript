dccbf93952bd6a77b4df2eda0eb881b946b644aa
unversal18 <geosil93@yahoo.com>
2018-11-07 20:28:05 -0800

    dayTwo lesson complete

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// console.log typo (lucky number)
+// console.log(log(name     nameTwo
+var firstName =  "george";
+    console.log(firstName);
+    let name "Ben"
+    let  name    "Norma"
+    const name "Norma"
+    const+"Hello my name is"
+    const name= Bob":
+
+    Console. (greeting= name";
+    )``.log(date):
+    const.log(date):
+    console.log"dsfdfrertrterterttrytryrtytry.length
+    console.log(str.length
+        
+        consol obj e.log(num= new Numbder(42):
+        const array={1.2.3});
+        Console.LOG(ARRAY2.LENGTH):
+    constprompt
+    const real num +parssient(favnum):
+    console.logrealNum):
+    
