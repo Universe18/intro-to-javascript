b28ff73423ab3b49b16eb12924dac312dc931197
unversal18 <geosil93@yahoo.com>
2018-11-25 15:03:51 -0800

    todays notes 11/25/18

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,62 @@
-console.log(42 ==42}:
-    CONST OBJ1+{
-        NAME:"bEN"
+// console.log(42 ==42}:
+//     CONST OBJ1+{
+//         NAME:"bEN"
 
-        Console.LOG(OBJ1 ==obj2):.tostring()
-        const arr1=(CAT'):
-        CONST ARR2 =(Cat):
+//         Console.LOG(OBJ1 ==obj2):.tostring()
+//         const arr1=(CAT'):
+//         CONST ARR2 =(Cat):
+//     if(LASTNAME)}
+//     if(lastName)}
+//     cdomcole.log"you did not provide a name')
+//     const username= prompt("what is your name?)":
+
+//     if uerName)}
+//     SVGPathSegLinetoRel(userName):
+//     else)}
+//     const geekwise=['Web for beginners', true) ('intro to js',
+//     console.log star wars', 
+//     console.log(7);)
+//     console.log (a major star waras fan';)
+//     constluckyNums =(2,3,4,5,6,7,8);
+//     undefined
+//     luckynums, index(20);
+//     luckyNums, lastIndexOf(2);
+//     arr.slice(567/ 1);
+//     const str='racecar';
+    
+//     const favMovies [the godfather. "micheal"];
+//     const favmovies [star wars."luke'];
+//     const favMovies [el norte. "greg nava"];
+//     const favmovies [apocalypse now. "colonel kurtz'];
+//     const favMovies 
+//     const favortieMovies (favMovies1, favMovie2, favMovie3)favMovie4);
+//     alert(favortiteMovies.length0);
+//     let i is > or = 10; i =) {}
+//     console.log(i);
+//     const=(sasha, igor, vladimr,);
+//     let i=0,1=names.length) i++)
+//     console.log((names{1}==)
+//     for(name of name);
+//     console.log(name);
+//     name.map(name=);
+//     if(albulm==='led zeplin'11')
+//     const person={
+//     name; ben;
+//     age 29
+//     };
+//     console.log(person.name)
+//     cons user={};
+
+//     user.name=prompt("Give me your name");
+//     user.age=prompt("give me your age");
+//     console.log(user);
+
+
+    
+    let movies=[['The Godfather', 1975], ['Apocolyse Now', 1979], ['The Terminator', 1984]];
+    
+    let i;
+    for (i = 0; i < movies.length; i++) {
+        console.log(movies.length);
     }
+   
